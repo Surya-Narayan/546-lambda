6c568a03c1f3dcc5bb2c0da6932293c917907ed0
Swanand Kavitkar <36107785+S-Threepio@users.noreply.github.com>
2023-03-22 20:11:25 -0700

    Update workload.py

--- workload.py
+++ workload.py
@@ -1,8 +1,8 @@
 from boto3 import client as boto3_client
 import os
 
-input_bucket = "546proj2"
-output_bucket = "546proj2output"
+input_bucket = "paas-input-bucket"
+output_bucket = "paas-output-bucket"
 test_cases = "test_cases/"
 
 def clear_input_bucket():
